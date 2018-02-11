b9a4de5debccafb86cc52bf003ddc039e373c138
Zhu Jiongyao <zjywade@qq.com>
2018-02-11 14:38:06 +0800

    add others

---
+++ essential.vim
@@ -0,0 +1,3 @@
+# <vim 实用技巧>中要用的vim配置
+set nocompatible
+filetype plugin on
---
+++ test_vim.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+"""for testing vim"""
+
+def test_it():
+    print('test_it')
+
+if __name__ == '__main__':
+    test_it()
